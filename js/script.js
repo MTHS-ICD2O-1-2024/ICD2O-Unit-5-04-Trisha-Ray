0f47765097674a6a895d3354d41576bc52b1697d
default <trisha.ray@stu.ocsb.ca>
2025-04-29 17:06:26 +0000

    java script done

--- js/script.js
+++ js/script.js
@@ -1,33 +1,26 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Trisha Ray
-// Created on: Feb 2025
+// Created on: April 2025
 // This file contains the JS functions for index.html
 'use strict'
 
 function calculate() {
-  // input
-  const movieRating = document.getElementById("movieforage").value
-
   // process
-  if (movieRating >= 17) {
+  const theAge = parseInt(document.getElementById("age").value);
+  const ticketOne = document.getElementById("option-Tuesday").checked;
+  const ticketTwo = document.getElementById("option-Thursday").checked;
+
+  // input
+  if  (ticketOne || ticketTwo || (theAge > 12 && theAge < 21)) {
+  
     // output
+  
     document.getElementById("results").innerHTML =
-      "You can see R rated movies."
-  }
-
-  else if (movieRating >= 13) {
-    document.getElementById("results").innerHTML =
-      "You can see PG-13 rated movies."
-  }
-
-  else if (movieRating >= 5) {
-    document.getElementById("results").innerHTML =
-      "You can see G and PG rated movies."
-  }
-
+      "<p>You are eligible for a student discount.</p>";
+  } 
   else {
     document.getElementById("results").innerHTML =
-      "Uhh, you are to young to see most things."
+      "<p>You must pay full price.</p>";
   }
 }
